19945ae94779f3f636ea09d0a2518f9238b6e52a
Alam <mohamed.masood.alam@philips.com>
2018-02-04 09:23:43 +0530

    Combine reducers, add modals

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from  './components/App'
 import registerServiceWorker from './registerServiceWorker';
 import {createStore} from 'redux'
+import {Provider } from 'react-redux'
+
 import reducer from './reducers'
 
 const store = createStore(reducer,
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
-console.log(store)
+// console.log(store)
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+  <App/>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
